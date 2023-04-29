8018822113f6cad8ea591cf0da420720718226ab
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-04-29 10:07:43 -0300

    up

---
+++ Funcionaraio/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import entities.Funcionario;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Funcionario funcionario = new Funcionario();
+        System.out.println();
+
+        System.out.printf("Name: ");
+        funcionario.name = s.nextLine();
+
+        System.out.printf("Gross salary: ");
+        funcionario.grossSalary = s.nextDouble();
+
+        System.out.printf("Tax: ");
+        funcionario.tax = s.nextDouble();
+
+        System.out.printf("%nEmployee: %s, $ %.2f%n", funcionario.name, funcionario.netSalary());
+
+        System.out.printf("%nWhich percentage to increase salary? ");
+        funcionario.increaseSalary(s.nextDouble());
+
+        System.out.printf("%nUpdate data: %s, $ %.2f%n", funcionario.name, funcionario.netSalary());
+
+
+    }
+}
